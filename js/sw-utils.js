c660ef5b81606f035134bb06218941cc1fb9eaa0
rodwyn <rodwynmoreno@gmail.com>
2021-05-26 19:09:21 -0500

    Implemented cache load with network fallback.

---
+++ js/sw-utils.js
@@ -0,0 +1,13 @@
+function updateDynamicCache(dynamicCache, request, response) {
+  if (response.ok) {
+    return caches.open(dynamicCache).then(
+      cache => {
+        cache.put(request, response.clone());
+
+        return response.clone();
+      }
+    );
+  } else {
+    return response;
+  }
+}
--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const STATIC_CACHE = 'static-v1';
+importScripts('js/sw-utils.js');
+
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -12,7 +14,8 @@ const APP_SHELL = [
   'img/avatars/spiderman.jpg',
   'img/avatars/thor.jpg',
   'img/avatars/wolverine.jpg',
-  'js/app.js'
+  'js/app.js',
+  'js/sw-utils.js'
 ];
 
 const APP_SHELL_INMUTABLE = [
@@ -39,5 +42,19 @@ self.addEventListener('activate', event => {
   });
 
   event.waitUntil(response);
+});
+
+self.addEventListener('fetch', event => {
+  const response = caches.match(event.request).then(resp => {
+    if (resp) {
+      return resp;
+    } else {
+      return fetch(event.request).then(fetchResponse => {
+        updateDynamicCache(DYNAMIC_CACHE, event.request, fetchResponse);
+      });
+    }
+  });
+
+  event.respondWith(response);
 });
 
